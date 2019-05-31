547833f4f96c8c66e75b4631bea24c474256f147
Edward Nutting <edward.nutting@bristol.ac.uk>
2019-05-31 01:02:57 +0100

    UART wrapper for PMOD BT2 (Bluetooth) device
    Enables zero-buffered, send/receive of bytes. Useful wrapper interface.

---
+++ PMOD.srcs/sources_1/new/PMOD_BT2.v
@@ -0,0 +1,287 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Engineer: Ec Nutting 
+// 
+// Create Date: 30.05.2019 15:12:07
+// Design Name: Top-level PMOD demo
+// Module Name: top
+// Project Name: PMOD
+// Target Devices: Zedboard (Zynq 7020)
+// Tool Versions: Vivado 2016.1
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// Licensed under MIT license - refer to LICENSE file.
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+module PMOD_BT2 (
+    input CLK,              // Clock - see also CLKS_PER_BAUD
+    input RST_N,            // Active-low reset
+    
+    input  RTS,             // BT2 Ready-To-Send (Pin 1) 
+    output reg RXD,         // BT2 RXD (Pin 2)
+    input  TXD,             // BT2 TXD (Pin 3)
+    output reg CTS,         // BT2 CTS (Pin 4)
+    input  STS,             // BT2 Connection status (Pin 7)
+    output wire RST_N_Out,  // BT2 Reset (active low) (Pin 8)
+    
+    input [7:0] char_in,        // Character (/byte) to send
+    output reg [7:0] char_out,  // Character (/byte) received
+    input send,                 // Active-high. Set to send. Keep high until 'sent' set.
+    output reg sent,            // Active-high. Pulses for 1 clock cycle when 'char_in' finished sending.
+    output reg receive,         // Active-high. Set while char_out valid when a byte is received.
+    input received              // Active-high. Set for 1 clock cycle to clear 'receive' and being accepting next character.
+    );
+    
+    parameter BAUD = 115200;
+    parameter CLK_FREQ = 100000000; // 100MHz
+    localparam CLKS_PER_BAUD = CLK_FREQ / BAUD;
+    
+    localparam [1:0] STATE_IDLE = 0;
+    localparam [1:0] STATE_START = 1;
+    localparam [1:0] STATE_BIT = 2;
+    localparam [1:0] STATE_STOP = 3;
+
+    assign RST_N_Out = RST_N;
+    
+    reg [1:0] send_state;
+    reg [2:0] send_bitnum;
+    reg [31:0] send_count;
+    
+    reg [1:0] receive_state;
+    reg [2:0] receive_bitnum;
+    reg [31:0] receive_count;
+    
+    // Double buffer as a low-quality but good-enough way to cross clock domains
+    //  Solves frequent metastability issue when receiving characters
+    reg TXD_buf1;
+    reg TXD_buf2;
+    always @(posedge CLK) begin
+        TXD_buf1 <= TXD;
+        TXD_buf2 <= TXD_buf1;
+    end
+    
+    // Trigger 'sent' signal
+    always @(posedge CLK) begin
+        if (send) begin
+            if (send_state == STATE_STOP && send_count == 0) begin
+                sent <= 1;
+            end
+        end
+        else begin
+            sent <= 0;
+        end
+    end
+    
+    // Ready to start sending when signalled to and BT2 is ready
+    // Note: RTS active low for BT2
+    wire start_send = send && !sent && !RTS;
+    
+    // Send timing/state control
+    always @(posedge CLK) begin
+        if (!RST_N) begin
+            send_state <= STATE_START;
+            send_bitnum <= 0;
+            send_count <= 0;
+        end
+        // Wait (approx.) duration of a bit in the BT2's clock domain
+        else if (send_count == 0) begin
+            case (send_state)
+                // Shouldn't end up here but anyway...
+                STATE_IDLE: begin
+                    send_state <= STATE_START;
+                end
+                
+                // Send the start bit
+                STATE_START: begin
+                    if (start_send) begin
+                        send_state <= STATE_BIT;
+                        send_bitnum <= 0;
+                        send_count <= CLKS_PER_BAUD;
+                    end
+                    else begin
+                        send_state <= STATE_START;
+                        send_bitnum <= 0;
+                        send_count <= 0;
+                    end
+                end
+
+                // Send the 8 data bits
+                STATE_BIT: begin
+                    if (send_bitnum < 7) begin
+                        send_bitnum <= send_bitnum + 1;
+                    end
+                    else begin
+                        send_bitnum <= 0;
+                        send_state <= STATE_STOP;
+                    end
+                    send_count <= CLKS_PER_BAUD;
+                end
+                
+                // Send the stop bit
+                STATE_STOP: begin
+                    send_state <= STATE_START;
+                    send_count <= CLKS_PER_BAUD;
+                end
+            endcase
+        end
+        else if (send_count > 0) begin
+            send_count <= send_count - 1;
+        end
+    end
+    
+    // Data line control
+    always @(posedge CLK) begin
+        if (!RST_N) begin
+            // 0 = Start bit, so drive high to avoid data transmission
+            RXD <= 1;
+        end
+        else begin
+            // Wait (approx.) duration of a bit in the BT2's clock domain
+            if (send_count == 0) begin
+                case (send_state)
+                    // Send the start bit
+                    STATE_START: begin
+                        if (start_send) begin
+                            RXD <= 0;
+                        end
+                        else begin
+                            RXD <= 1;
+                        end
+                    end
+
+                    // Send a data bit
+                    STATE_BIT: begin
+                        RXD <= char_in[send_bitnum];
+                    end
+                    
+                    // Send the stop bit
+                    STATE_STOP: begin
+                        RXD <= 1;
+                    end
+                endcase
+            end
+        end
+    end
+    
+    // Ready to start receiving when the BT2 sends the start bit and any previous byte has been processed
+    wire start_receive = TXD_buf2 == 0 && !receive && !received;
+
+    // Receiving timing/state control
+    always @(posedge CLK) begin
+        if (!RST_N) begin
+            receive_state <= STATE_START;
+            receive_bitnum <= 0;
+            receive_count <= 0;
+        end
+        // Wait (approx.) duration of a bit in the BT2's clock domain
+        else if (receive_count == 0) begin
+            case (receive_state)
+                // Shouldn't end up here but anyway...
+                STATE_IDLE: begin
+                    receive_state <= STATE_START;
+                end
+                
+                // Wait for and receive the start bit
+                STATE_START: begin
+                    if (start_receive) begin
+                        receive_bitnum <= 0;
+                        receive_count <= CLKS_PER_BAUD + (CLKS_PER_BAUD / 3);
+                        receive_state <= STATE_BIT;
+                    end
+                end
+
+                // Receive data bits
+                STATE_BIT: begin
+                    if (receive_bitnum < 7) begin
+                        receive_bitnum <= receive_bitnum + 1;
+                    end
+                    else begin
+                        receive_bitnum <= 0;
+                        receive_state <= STATE_STOP;
+                    end
+
+                    receive_count <= CLKS_PER_BAUD;
+                end
+
+                // Receive stop bit
+                //  Then wait a bit of extra time before returning to START
+                //  This ensures CTS goes low after the stop bit has finished transmitting
+                STATE_STOP: begin
+                    receive_state <= STATE_START;
+                    receive_count <= 3 * (CLKS_PER_BAUD / 4);
+                end
+            endcase
+        end
+        else if (receive_count > 0) begin
+            receive_count <= receive_count - 1;
+        end
+    end
+    
+    // Data line input
+    always @(posedge CLK) begin
+        if (!RST_N) begin
+            char_out <= 0;
+        end
+        // Wait (approx.) duration of a bit in the BT2's clock domain
+        else if (receive_count == 0) begin
+            case (receive_state)
+                // Receive the start bit
+                STATE_START: begin
+                    if (start_receive) begin
+                        char_out <= 0;
+                    end
+                end
+
+                // Receive a data bit
+                STATE_BIT: begin            
+                    char_out[receive_bitnum] <= TXD_buf2;
+                end
+                
+                // Stop bit is handled below
+            endcase
+        end
+    end
+    
+    always @(posedge CLK) begin
+        if (!RST_N) begin
+            receive <= 0;
+        end
+        else begin
+            // If supposed to be receiving stop bit
+            if (receive_state == STATE_STOP && receive_count == 0) begin
+                // Check for stop bit
+                if (TXD_buf2) begin
+                    // Successful byte receive
+                    receive <= 1;
+                end
+                else begin
+                    // Fail. Wasn't a proper byte.
+                    receive <= 0;
+                end
+            end
+            // Wait for `received` to indicate safe to receive next char
+            else if (receive) begin
+                if (received) begin
+                    receive <= 0;
+                end
+            end
+        end
+    end
+
+    // Clear-To-Send control
+    // Note: CTS active low for BT2
+    always @(posedge CLK) begin
+        if (!RST_N) begin
+            // Never clear to send while being reset as we can't receive anything 
+            CTS <= 1;
+        end
+        else begin
+            // CTS when finished fully receiving/accepting previous byte
+            CTS <= receive_state != STATE_START || receive_count > 0 || receive || received;
+        end
+    end
+    
+endmodule
---
+++ PMOD.srcs/sources_1/new/Top.v
@@ -0,0 +1,134 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Engineer: Ec Nutting 
+// 
+// Create Date: 30.05.2019 15:12:07
+// Design Name: Top-level PMOD demo
+// Module Name: top
+// Project Name: PMOD
+// Target Devices: Zedboard (Zynq 7020)
+// Tool Versions: Vivado 2016.1
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// Licensed under MIT license - refer to LICENSE file.
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+module top(
+    input GCLK, // Clock
+    input BTNC, // Button center (reset all when pressed)
+    input BTND, // Button down   (change LED 3 blink speed)
+    input BTNU, // Buttown up    (transmit fixed text)
+
+    output LD0, // LED 0
+    output LD1, // LED 1
+    output LD2, // LED 2
+    output LD3, // LED 3
+    output LD4, // LED 4
+    output LD5, // LED 5
+    output LD6, // LED 6
+    output LD7, // LED 7
+
+    // PMOD BT2 attached to PMOD Port B (JB1-JB12)
+    input  JB1, // RTS
+    output JB2, // RXD
+    input  JB3, // TXD
+    output JB4, // CTS
+    // JB5/6 - Power/ground
+    input  JB7, // Connection status
+    output JB8, // Reset
+    input  JB9, // Not connected
+    input  JB10 // Not connected
+    // JB11/12 - Power/ground
+    );
+    
+    // See constraints file - global clock should be from Y9 package pin @ 100MHz
+    wire CLK = GCLK;
+
+    // Reset-low signal - reset when button is pressed
+    wire RST_N = !BTNC;
+    
+    // De-bounced switches
+    //  Useful for debugging - use to control `bt2.RST` and `bt2.received`
+    //      Note: When debugging, remember to isolate BT2_RST_N from the BT2 module using: 
+    //                assign BT2_RST_N = RST_N;
+    //            And changing BT2_RST_N to BT2_RST_N_Unused in bt2 declaration.
+
+    /*DeBounce BTND_debounce (
+        .clk(CLK),
+        .n_reset(RST_N),
+        .button_in(BTND),
+        .DB_out(BTND_pressed)
+        );
+
+    reg BTND_pressed_prev;
+    always @(posedge CLK) begin
+        BTND_pressed_prev <= BTND_pressed;
+    end
+    
+    DeBounce BTNU_debounce (
+        .clk(CLK),
+        .n_reset(RST_N),
+        .button_in(BTNU),
+        .DB_out(BTNU_pressed)
+        );
+
+    reg BTNU_pressed_prev;
+    always @(posedge CLK) begin
+        BTNU_pressed_prev <= BTNU_pressed;
+    end
+    */
+    
+    // BT2 input signals
+    assign BT2_RTS = JB1;
+    assign BT2_TXD = JB3;
+    assign BT2_STS = JB7;
+
+    // BT2 indicator LEDs
+    assign LD0 = BT2_STS;
+    // LEDs for debug
+    /*
+    assign LD1 = BT2_RXD;
+    assign LD2 = BT2_TXD;
+    assign LD3 = BT2_RTS;
+    assign LD4 = BT2_CTS;
+    assign LD5 = BT2_receive;
+    assign LD6 = BT2_sent;
+    */
+
+    // BT2 outputs
+    assign JB2 = BT2_RXD;
+    assign JB4 = BT2_CTS;
+    assign JB8 = BT2_RST_N;
+
+    wire [7:0] BT2_char_out;
+    wire [7:0] BT2_char_in;
+
+    // BT2 module
+    PMOD_BT2 bt2 (
+        .CLK        (CLK),
+        .RST_N      (RST_N),
+
+        .RTS        (BT2_RTS),
+        .RXD        (BT2_RXD),
+        .TXD        (BT2_TXD),
+        .CTS        (BT2_CTS),
+        .STS        (BT2_STS),
+        .RST_N_Out  (BT2_RST_N),
+        
+        .char_in    (BT2_char_in),
+        .char_out   (BT2_char_out),
+        .send       (BT2_send),
+        .sent       (BT2_sent),
+        .receive    (BT2_receive),
+        .received   (BT2_received)
+        );
+        
+    // Configure BT2 as an echo device
+    assign BT2_char_in = BT2_char_out;
+    assign BT2_send = BT2_receive && !BT2_sent;
+    assign BT2_received = BT2_sent;
+
+endmodule
